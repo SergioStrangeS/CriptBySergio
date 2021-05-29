ad416cc5fc5d25165309fd3c55e09f10c632c5bf
Sergey Egorov <ser.12egorov@gmail.com>
2021-05-29 18:18:37 +0300

    доработка

--- CriptBySergo/Form1.cs
+++ CriptBySergo/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CriptBySergo;
+using System;
 using System.Windows.Forms;
 
 namespace EncryptString {
@@ -7,39 +8,34 @@ namespace EncryptString {
             InitializeComponent();
         }
 
-        public string encrypt;
-        public int key = 2019;                                          //Указываем переменную ключа.
 
-        private void button1_Click(object sender, EventArgs e)
+        public void Clear()
         {
-            char[] arr = textBox1.Text.ToCharArray();                   //Разбираем строку на символы и записываем в массив.
-            encrypt = "";
-            for (int i = 0; i < arr.Length; i++)
-            {
-                encrypt += ((int)arr[i] * key).ToString() + '-';        //Приводим символ к типу int,умножаем на (переменная key) и разделяем каждый "шифрованный символ символом '-'. 
-            }
-
-            textBox2.Text = encrypt;                                    //заполняем textBox2
-            textBox1.Clear();                                           //Очистка textBox'ов
+            textBox1.Clear();
             textBox3.Clear();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = textBox1.Text;
+            var key = int.Parse(textBox4.Text);
 
-        private void button2_Click_1(object sender, EventArgs e) {
-            string[] arr = textBox2.Text.Split('-');                    //Наши "символы же разделены '-', вот и ищем их.
-            for (int i = 0; i < arr.Length; i++) {
-                try {
-                    textBox3.Text += ((char)(Convert.ToInt32(arr[i]) / key)).ToString();//Тут вот муть какая-то, обернул в try, catch. Делим обратно на 2019 и приводим к типу char.
-                }
+            var encoding = Cript.Encode(text, key);
+            textBox2.Text = encoding;
 
-                catch {
-                }
-            }
-            textBox2.Clear();                                           //Очистка textBox'ов
+            Clear();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            var key = int.Parse(textBox4.Text);
+            var text = textBox2.Text;
+            var decoding = Cript.Decode(text, key);
+            textBox3.Text = decoding;
         }
 
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            key = int.Parse(textBox4.Text);
+            /**/
         }
 
         private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
